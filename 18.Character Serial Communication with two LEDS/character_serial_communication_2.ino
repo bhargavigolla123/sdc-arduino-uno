e274e2adfd04e46a3a637393970dad56ec882274
bhargavigolla123 <bhargavigolla2004@gmail.com>
2024-10-19 18:37:24 +0530

    18.Character Serial Communication with two LEDS

---
+++ 18.Character Serial Communication with two LEDS/character_serial_communication_2.ino
@@ -0,0 +1,18 @@
+// C++ code
+//
+void setup()
+{
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  Serial.println("a");
+  delay(2000);
+  Serial.println("b");
+  delay(2000);
+  Serial.println("c");
+  delay(2000);
+  Serial.println("d");
+  delay(2000);
+}
